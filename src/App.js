95a46118be59bb0ce251834404e3fb1ae6dab32e
Daniel Deutsch <danieldeutsch@Daniels-MacBook-Pro.local>
2018-05-16 23:39:09 +0200

    title fix part 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/Navbar.js'
 import EscortContainer from './components/EscortContainer.js'
+import BasicExample from './components/Backend.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Title />
         <EscortContainer />
-        <ProfilUser/>
+        <BasicExample />
       </div>
     );
   }
 }
+
+
 
 export default App
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+class Title extends Component {
+    state = {  }
+    render() {
+        return (
+            <h1 ClassName= "MainTitle">Find your intergalatic escort </h1>
+        );
+    }
+}
+
+export default Title;
