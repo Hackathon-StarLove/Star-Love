5118e7e122937e1a04c85b6e94e0f0706705ec39
Lor Chtg <lorraine.chastang@gmail.com>
2018-05-16 22:13:24 +0200

    styles for Escor and EscortContainer component

--- src/components/EscortContainer.js
+++ src/components/EscortContainer.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import Escort from './Escort.js'
+import './css/escort.css'
 
 const EscortContainer = (props) => {
     return <div className="container">
-
+        <h1>Find your intergalactic escort</h1>
+        <Escort />
+        <Escort />
+        <Escort />
+        <Escort />
     </div>
 }
 
