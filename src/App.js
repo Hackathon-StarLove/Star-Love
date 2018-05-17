a682c84ff00213339bc2a72f81a44a6b5305d90a
Novyrok <novyrok@localhost.localdomain>
2018-05-17 04:24:15 +0200

    component user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
 import Navbar from './components/Navbar.js'
-import EscortContainer from './components/EscortContainer.js'
+//import EscortContainer from './components/EscortContainer.js'
+import ProfilUser from './components/ProfilUser.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <EscortContainer />
-        <ProfilUser/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar/>
+                <ProfilUser/>
+              
+            </div>
+        );
+    }
 }
 
 export default App
