427c95da5312977346d38beaed60f4b9763dc309
Daniel Deutsch <danieldeutsch@Daniels-MacBook-Pro.local>
2018-05-17 04:16:19 +0200

    fixed conflict userProfil

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import './App.css'
 import Avatar from './components/Avatar.js'
@@ -10,33 +10,29 @@ import Navbar from './components/Navbar.js'
 import Title from './components/Title.js'
 import All from './components/Routes.js'
 
-
 class App extends Component {
-  state = {
-    data: []
-  }
-  async componentDidMount() {
-    const response = await fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
-    const json = await response.json()
-    this.setState({ data: json })
-  }
-  render() {
-    console.log(this.state.data)
-    return (
-      <div className="App">
-//       <Avatar />
-//       <MainPanel />
-//       <UserProfile />
-//       <Reserve name="Reserver"/>
-        <Navbar />
-        <Title text="Find your intergalatic escort"/>
-
-        {/* <BasicExample /> */}
-        <All char={this.state.data} />
-      </div>
-    )
-  }
+    state = {
+        data: []
+    }
+    async componentDidMount() {
+        const response = await fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
+        const json = await response.json()
+        this.setState({data: json})
+    }
+    render() {
+        console.log(this.state.data)
+        return (
+            <div className="App">
+                {/* <Avatar/>
+                <MainPanel/>
+                <UserProfile/>
+                <Reserve name="Reserver"/> */}
+                <Navbar/>
+                <Title text="Find your intergalatic escort"/> {/* <BasicExample /> */}
+                <All char={this.state.data}/>
+            </div>
+        )
+    }
 }
 
 export default App
-
