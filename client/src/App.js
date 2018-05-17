091fae8d681edf69e2342c0297e1a4765394495b
Lor Chtg <lorraine.chastang@gmail.com>
2018-05-18 01:26:33 +0200

    navbar's icons added

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,11 @@ import './components/css/Navbar.css'
 import './components/css/escort.css'
 import './components/css/profile.css'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import logo from './img/star-love-02.png'
+import escortbook from './img/star-love-04.png'
+import myprofile from './img/star-love-03.png'
+import reservation from './img/star-love-05.png'
+import logout from './img/star-love-06.png'
 
 class App extends Component {
     state = {
@@ -31,31 +36,31 @@ class App extends Component {
                 <div className="App">
                     <nav>
                         <div className="logo">
-                            <Link to="/"><img src="http://via.placeholder.com/130x80" alt="Logo"/></Link>
+                            <Link to="/"><img src={logo} alt="Logo"/></Link>
                         </div>
                         <div className="nav-item">
                             <ul>
                                 <li>
-                                    <Link to="/escorts"><img src="http://via.placeholder.com/70x70" alt="Escorts book"/>
+                                    <Link to="/escorts"><img src={escortbook} alt="Escorts book"/>
                                         <p>Escorts book</p>
                                     </Link>
                                 </li>
                                 <li>
                                     <Link to="myprofil">
-                                        <img src="http://via.placeholder.com/70x70" alt="My profile"/>
+                                        <img src={myprofile} alt="My profile"/>
                                         <p>My profile</p>
                                     </Link>
                                 </li>
                                 <li>
 								<Link to="reservation">
-                                    <img src="http://via.placeholder.com/70x70" alt="My reservations"/>
+                                    <img src={reservation} alt="My reservations"/>
                                     <p>My reservations</p>
 									</Link>
                                 </li>
                             </ul>
                         </div>
                         <div className="logout">
-                            <img src="http://via.placeholder.com/40x40" alt="Log out"/>
+                            <img src={logout} alt="Log out"/>
                             <p>Log out</p>
                         </div>
                     </nav>
