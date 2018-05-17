1b75facd5792f8587467327c9262956fb1d7bd3b
fenrisl <daniel.deutsch25@gmail.com>
2018-05-17 15:50:38 +0200

    blop

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,71 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
-import Navbar from './components/Navbar.js'
+// import Navbar from './components/Navbar.js'
 import UserProfil from './components/UserProfile.js'
 import Reserve from './components/ReserveButton.js'
 // import BasicExample from './components/Backend.js'
 import Title from './components/Title.js'
-import All from './components/Routes.js'
+import All from './components/All.js'
+import './components/css/Navbar.css'
+import './components/css/escort.css'
+import './components/css/profile.css'
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 class App extends Component {
-  state = {
-    data: [],
-    test: ''
-  }
-  async componentDidMount() {
-    const response = await fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
-    const json = await response.json()
-    this.setState({ data: json })
-    const response1 = await fetch('http://localhost:3030')
-    const json1 = await response1.json()
-    this.setState({ test: json1 })
-  }
+    state = {
+        data: [],
+        test: ''
+    }
+    async componentDidMount() {
+        const response = await fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
+        const json = await response.json()
+        this.setState({data: json})
+        const response1 = await fetch('http://localhost:3030')
+        const json1 = await response1.json()
+        this.setState({test: json1})
+    }
 
-  render() {
-	console.log(this.state.data)
-	console.log(this.state.test)
-    return (
-      <div className="App">
-        <Navbar />
-        <Title text="Find your intergalatic escort"/>
-		
-        <UserProfil />
-		<Reserve name="reserve"/>
-        {/* <BasicExample /> */}
-        <All char={this.state.data} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <nav>
+                        <div className="logo">
+                            <Link to="/"><img src="http://via.placeholder.com/130x80" alt="Logo"/></Link>
+                        </div>
+                        <div className="nav-item">
+                            <ul>
+                                <li>
+                                    <Link to="/escorts"><img src="http://via.placeholder.com/70x70" alt="Escorts book"/>
+                                        <p>Escorts book</p>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="myprofil">
+                                        <img src="http://via.placeholder.com/70x70" alt="My profile"/>
+                                        <p>My profile</p>
+                                    </Link>
+                                </li>
+                                <li>
+								<Link to="reservation">
+                                    <img src="http://via.placeholder.com/70x70" alt="My reservations"/>
+                                    <p>My reservations</p>
+									</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="logout">
+                            <img src="http://via.placeholder.com/40x40" alt="Log out"/>
+                            <p>Log out</p>
+                        </div>
+                    </nav>
+                    <Route path="/escorts" component={All} char={this.state.data}/>
+                    <Route exact path="/myprofil" component={UserProfil}/>
+					<Route exact path="/reservation" component={UserProfil}/>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App
---
+++ client/src/components/All.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import './css/Navbar.css'
+import './css/escort.css'
+import './css/profile.css'
+
+const createCharacterElement = character => {
+    return (
+        <div>
+            <a
+                className="character"
+                style={{
+                backgroundImage: `url(${character.pic})`
+            }}
+                href="profile.html?id==={character.id}">
+                <div className="character-infos">
+                    <div className="character-name">{character.name}</div>
+                    <div className="character-github"><img src={character.image}/>
+                    </div>
+                </div>
+            </a>
+        </div>
+    )
+}
+
+// const render = characters => {
+// characters.map(createCharacterElement).join('')} // style={{marginRight:
+// spacing + 'em'}}
+// fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
+// .then(res => res.json())   .then() export default All
+
+const API = 'https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json';
+
+class All extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        };
+    }
+    async componentDidMount() {
+        const response = await fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
+        const json = await response.json()
+        this.setState({data: json})
+    }
+
+    render() {
+        return (
+            <div> {this.state.data.map(createCharacterElement)}
+            </div>
+        )
+    }
+
+    // const All = ({char}) => {
+    //     if (char.length === 0) {
+    //         return <div>Loading Results</div>
+    //     }
+
+    //     return <div>{char.map(createCharacterElement)}</div>
+    }
+
+    export default All
