cce4e504d56c606fa51728740f9db2229eae9f7f
fenrisl <daniel.deutsch25@gmail.com>
2018-05-18 06:13:48 +0200

    yaya

---
+++ client/src/App.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import UserProfil from './components/UserProfile.js'
+import Reserve from './components/ReserveButton.js'
+import Title from './components/Title.js'
+import { EscortContainer, SelectionContainer } from './components/EscortContainer.js'
+import SimpleForm from './components/form.js'
+import './App.css'
+import './components/css/Navbar.css'
+import './components/css/escort.css'
+import './components/css/profile.css'
+import logo from './img/star-love-02.png'
+import escortbook from './img/star-love-04.png'
+import myprofile from './img/star-love-03.png'
+import reservation from './img/star-love-05.png'
+import logout from './img/star-love-07.png'
+
+class App extends Component {
+    state = {
+        data: [],
+        test: ''
+    }
+    async componentDidMount() {
+        const response = await fetch('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
+        const json = await response.json()
+        this.setState({data: json})
+        const response1 = await fetch('http://localhost:3030')
+        const json1 = await response1.json()
+        this.setState({test: json1})
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <nav>
+                        <div className="logo">
+                            <Link to="/"><img src={logo} alt="Logo"/></Link>
+                        </div>
+                        <div className="nav-item">
+                            <ul>
+                                <li>
+                                    <Link to="/escorts"><img src={escortbook} alt="Escorts book"/>
+                                        <p>Escorts book</p>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="myprofil">
+                                        <img src={myprofile} alt="My profile"/>
+                                        <p>My profile</p>
+                                    </Link>
+                                </li>
+                                <li>
+								<Link to="reservation">
+                                    <img src={reservation} alt="My reservations"/>
+                                    <p>My reservations</p>
+									</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="logout">
+                            <img src={logout} alt="Log out"/>
+                            <p>Log out</p>
+                        </div>
+                    </nav>
+                    <Route exact path="/" component={SelectionContainer} char={this.state.data}/>
+                    <Route exact path="/escorts" component={EscortContainer} char={this.state.data}/>                    
+                    <Route exact path="/myprofil" component={UserProfil}/>
+					<Route exact path="/reservation" component={SimpleForm}/>
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default App
---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import Title from './Title.js'
+
+const UserProfile = ({char}) => {
+    return <div className="container">
+        <Title text="My Profil"/>
+        <div className="User">
+            <button className="Edit">
+                Edit
+            </button>
+        </div>
+        <div>
+
+            <div className="flex-container">
+                <div>
+                    <img className="ProfilePic" src="http://via.placeholder.com/250x250"/>
+
+                </div>
+                <div>
+                    <p>Name: Jane Doe</p>
+                    <p>Gender: Female</p>
+                    <p>Age: 24</p>
+                    <p>Affiliation: Empire</p>
+                </div>
+            </div>
+            <div className="Description"><h3>Description</h3><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>
+        </div>
+    </div>
+}
+
+export default UserProfile
