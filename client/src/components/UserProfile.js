5c465c3a950791f73b0c984e0feaa923bb4ef6c2
fenrisl <daniel.deutsch25@gmail.com>
2018-05-18 04:59:16 +0200

    profil part1

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,17 +1,31 @@
-import React, {Component} from 'react';
-import User from './user.js'
-import MainPanel from './MainPanel.js'
-import './css/profile.css'
+import React, {Component} from 'react'
+import Title from './Title.js'
 
+const EscortContainer = ({char}) => {
+    return <div className="container">
+        <Title text="My Profil"/>
+        <div className="User">
+            <button className="Edit">
+                Edit
+            </button>
+        </div>
+        <div>
 
-export class UserProfile extends React.Component {
-  render() {
-    return (
-      <div id="user-profile">
-        <MainPanel info={User} />
-      </div>
-    )
-  }
+            <div className="flex-container">
+                <div>
+                    <img className="ProfilePic" src="http://via.placeholder.com/250x250"/>
+
+                </div>
+                <div>
+                    <p>Name: Jane Doe</p>
+                    <p>Gender: Female</p>
+                    <p>Age: 24</p>
+                    <p>Affiliation: Empire</p>
+                </div>
+            </div>
+            <div className="Description"><h3>Description</h3><p>dweddeedew</p></div>
+        </div>
+    </div>
 }
 
-export default UserProfile
+export default EscortContainer
