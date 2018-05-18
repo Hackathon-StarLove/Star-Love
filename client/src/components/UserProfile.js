32a478946c7f5f6d70ab7106088ee355437d385c
fenrisl <daniel.deutsch25@gmail.com>
2018-05-18 05:25:31 +0200

    css profil

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -23,7 +23,10 @@ const EscortContainer = ({char}) => {
                     <p>Affiliation: Empire</p>
                 </div>
             </div>
-            <div className="Description"><h3>Description</h3><p>dweddeedew</p></div>
+            <div className="Description"><h3>Description</h3><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>
         </div>
     </div>
 }
